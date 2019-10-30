ec0889d5abdf6b3775b3051b0c9e69099d00e99c
Max Base <Max@Asrez.com>
2019-10-30 10:32:26 +0330

    Update test.c

--- test.c
+++ test.c
@@ -4,30 +4,30 @@
 #include <string.h>
 
 struct string {
-	char *ptr;
-	size_t len;
+	char *pointers;
+	size_t length;
 };
 
-void init_string(struct string *s) {
-	s->len = 0;
-	s->ptr = malloc(s->len+1);
-	if (s->ptr == NULL) {
+void initString(struct string *s) {
+	s->length = 0;
+	s->pointers = malloc(s->length+1);
+	if (s->pointers == NULL) {
 		fprintf(stderr, "malloc() failed\n");
 		exit(EXIT_FAILURE);
 	}
-	s->ptr[0] = '\0';
+	s->pointers[0] = '\0';
 }
 
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s) {
-	size_t new_len = s->len + size*nmemb;
-	s->ptr = realloc(s->ptr, new_len+1);
-	if (s->ptr == NULL) {
+size_t backFunction(void *pointers, size_t size, size_t nmemb, struct string *s) {
+	size_t new_length = s->length + size*nmemb;
+	s->pointers = realloc(s->pointers, new_length+1);
+	if (s->pointers == NULL) {
 		fprintf(stderr, "realloc() failed\n");
 		exit(EXIT_FAILURE);
 	}
-	memcpy(s->ptr+s->len, ptr, size*nmemb);
-	s->ptr[new_len] = '\0';
-	s->len = new_len;
+	memcpy(s->pointers+s->length, pointers, size*nmemb);
+	s->pointers[new_length] = '\0';
+	s->length = new_length;
 	return size*nmemb;
 }
 
@@ -37,20 +37,20 @@ int main(int argc, char const *argv[]) {
 	curl = curl_easy_init();
 	if(curl) {
 		struct string s;
-		init_string(&s);
+		initString(&s);
 		curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/");
 		curl_easy_setopt(curl, CURLOPT_POST, 1L);
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Max&username=BaseMax&sampleAge=53");
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, backFunction);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
  		res = curl_easy_perform(curl);
 		if(res != CURLE_OK) {
 			fprintf(stderr, "{\"status\":\"failed\"}");
 			return -1;
 		}
-		printf("%s\n", s.ptr);
-		free(s.ptr);
+		printf("%s\n", s.pointers);
+		free(s.pointers);
 		curl_easy_cleanup(curl);
 	}
 	return 0;
